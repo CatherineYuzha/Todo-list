114ef8997c76876a99206655ca495ffb4750a50e
Catherine Yuzhakova <yuuzhaa@gmail.com>
2019-12-11 19:05:37 +0300

    Adding item

--- src/js/todo.js
+++ src/js/todo.js
@@ -6,21 +6,23 @@ document.addEventListener("DOMContentLoaded", function() {
       edit: false
     },
     {
-      text: "заметка1",
+      text: "Заметка 1",
       done: false,
       edit: false
     },
     {
-      text: "заметка2",
+      text: "Заметка 2 ",
       done: false,
       edit: false
     }
   ];
 
-  const task = document.querySelector(".js-todo-list");
+  const $taskList = document.querySelector(".js-todo-list");
   let html = "";
+  const buttonAddTask = document.querySelector(".js-button");
+  const inputAddTask = document.querySelector(".js-input");
 
-  todos.map(
+  todos.forEach(
     item =>
       (html += `  <li class="todo-list__item">
       <p class="todo-list__item-text">${item.text}</p>
@@ -29,5 +31,17 @@ document.addEventListener("DOMContentLoaded", function() {
       <button class="todo-list__item-button">Удалить</button>
     </li> `)
   );
-  task.innerHTML = html;
+
+  $taskList.innerHTML = html;
+
+  buttonAddTask.addEventListener("click", function() {
+    todos.push({ text: inputAddTask });
+
+    $taskList.innerHTML += `  <li class="todo-list__item">
+      <p class="todo-list__item-text">${inputAddTask}</p>
+      <input class="todo-list__item-textfield" type="text" />
+      <button class="todo-list__item-button">Изменить</button>
+      <button class="todo-list__item-button">Удалить</button>
+    </li> `;
+  });
 });
