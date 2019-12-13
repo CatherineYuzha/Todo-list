660e720137ace8284a009cda91f1236b0ecbf694
Catherine Yuzhakova <yuuzhaa@gmail.com>
2019-12-13 18:18:07 +0300

    Fixed layout, done adding and deleting tasks

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,47 +1,75 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const todos = [
+  let tasks = [
     {
       text: "Заметка",
+      identifier: 1,
       done: true,
       edit: false
     },
     {
       text: "Заметка 1",
+      identifier: 2,
       done: false,
       edit: false
     },
     {
       text: "Заметка 2 ",
+      identifier: 3,
       done: false,
       edit: false
     }
   ];
-
   const $taskList = document.querySelector(".js-todo-list");
-  let html = "";
-  const buttonAddTask = document.querySelector(".js-button");
-  const inputAddTask = document.querySelector(".js-input");
+  const $buttonAddTask = document.querySelector(".js-button");
+  const $inputAddTask = document.querySelector(".js-input");
 
-  todos.forEach(
-    item =>
-      (html += `  <li class="todo-list__item">
-      <p class="todo-list__item-text">${item.text}</p>
-      <input class="todo-list__item-textfield" type="text" />
-      <button class="todo-list__item-button">Изменить</button>
-      <button class="todo-list__item-button">Удалить</button>
-    </li> `)
-  );
-
-  $taskList.innerHTML = html;
-
-  buttonAddTask.addEventListener("click", function() {
-    todos.push({ text: inputAddTask });
-
-    $taskList.innerHTML += `  <li class="todo-list__item">
-      <p class="todo-list__item-text">${inputAddTask}</p>
-      <input class="todo-list__item-textfield" type="text" />
-      <button class="todo-list__item-button">Изменить</button>
-      <button class="todo-list__item-button">Удалить</button>
-    </li> `;
+  tasks.forEach(item => {
+    $taskList.insertAdjacentHTML("beforeend", templateTask(item));
   });
+
+  const $buttons = document.querySelectorAll(".js-button-delete");
+
+  $buttonAddTask.addEventListener("click", function() {
+    if ($inputAddTask.value === "") {
+      return false;
+    }
+    const newTask = {
+      text: $inputAddTask.value,
+      done: false,
+      edit: false,
+      identifier: tasks.length + 1
+    };
+
+    tasks.push(newTask);
+    $taskList.insertAdjacentHTML("beforeend", templateTask(newTask));
+    $inputAddTask.value = "";
+    removeButton($taskList.lastElementChild.querySelector(".js-button-delete"));
+  });
+
+  $buttons.forEach(button => removeButton(button));
+
+  function removeButton(button) {
+    button.addEventListener("click", function(event) {
+      const data = event.target.dataset;
+
+      tasks = tasks.filter(item => item.dataPosition !== data.id);
+
+      const childrensTaskList = $taskList.children;
+
+      for (let task of childrensTaskList) {
+        if (task.dataset.id === data.id) {
+          task.remove();
+        }
+      }
+    });
+  }
+
+  function templateTask(task) {
+    return `  <li class="todo-list__item" data-id="${task.identifier}">
+      <p class="todo-list__item-text">${task.text}</p>
+      <input class="todo-list__item-textfield" type="text" />
+      <button class="todo-list__item-button">Изменить</button>
+      <button class="todo-list__item-button js-button-delete" data-id="${task.identifier}">Удалить</button>
+    </li> `;
+  }
 });
