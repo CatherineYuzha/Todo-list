00931f9b925cb0aab9e5bf9187e2deaf3ed10812
Catherine Yuzhakova <yuuzhaa@gmail.com>
2019-12-20 17:04:41 +0300

    Edits

--- src/js/AddTaskForm.js
+++ src/js/AddTaskForm.js
@@ -2,10 +2,21 @@ import Task from "./Task";
 
 export default class AddTaskForm {
   constructor(addTaskCallback) {
-    this.$addTaskInput = document.querySelector(".js-input");
-    this.$addTaskButton = document.querySelector(".js-button");
+    this.addTaskForm = document.createElement("div");
+    this.addTaskForm.classList.add("todo-form");
 
+    this.$addTaskInput = document.createElement("input");
+    this.$addTaskInput.classList.add("todo-form__input");
+    this.$addTaskInput.setAttribute("type", "text");
+
+    this.$addTaskButton = document.createElement("button");
+    this.$addTaskButton.classList.add("todo-form__button");
+    this.$addTaskButton.append(document.createTextNode("Добавить"));
+    this.$addTaskButton.setAttribute("onclick", "return false");
+
+    this.addTaskForm.append(this.$addTaskInput, this.$addTaskButton);
     this.$addTaskButton.onclick = this.assignAddButton.bind(this);
+    document.body.insertBefore(addTaskForm, script);
     this.addTask = addTaskCallback;
   }
 
